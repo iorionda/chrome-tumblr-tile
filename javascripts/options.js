3e54ad9201263143c7e2d76063847d91a0de99c2
Iori ONDA <iori.onda@me.com>
2013-02-15 11:30:16 +0900

    Remove hostname and api key settings from option

--- javascripts/options.js
+++ javascripts/options.js
@@ -1,21 +1,14 @@
 $(function() {
 
     tumblrTile.loadConfig();
-
-    var $apiKey    = $('#setting input[name="apiKey"]');
-    var $hostname  = $('#setting input[name="hostname"]');
     var $baseWidth = $('#setting input[name="baseWidth"]');
     var $margin    = $('#setting input[name="margin"]');
 
-    $apiKey.val(tumblrTile.config.apiKey);
-    $hostname.val(tumblrTile.config.hostname);
     $baseWidth.val(tumblrTile.config.baseWidth);
     $margin.val(tumblrTile.config.margin);
 
     $("#setting").submit(function() {
         var hash = {
-            apiKey   : $apiKey.val(),
-            hostname : $hostname.val(),
             baseWidth: parseInt($baseWidth.val()),
             margin   : parseInt($margin.val())
         };
